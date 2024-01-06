2d5969d6069735e9fe9e6ef8904121e4c33aaf08
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2024-01-06 04:34:59 +0100

    Added a separate method to create the ElevatedButton widget with common configurations and Extracted the bottom sheet showing logic into separate methods for buy and sell actions

--- lib/screens/utils/action_button_section.dart
+++ lib/screens/utils/action_button_section.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'bottom_sheet.dart';
 
 class ActionButtonSection extends StatelessWidget {
-  const ActionButtonSection({super.key});
+  const ActionButtonSection({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,74 +10,69 @@ class ActionButtonSection extends StatelessWidget {
       height: 64,
       width: MediaQuery.of(context).size.width,
       padding: const EdgeInsets.all(12),
-      clipBehavior: Clip.antiAlias,
-      decoration: ShapeDecoration(
+      decoration: BoxDecoration(
         color: Colors.white,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(width: 1, color: Color(0xFFF1F1F1)),
-        ),
+        border: Border.all(width: 1, color: const Color(0xFFF1F1F1)),
+        borderRadius: BorderRadius.circular(8.0),
       ),
-
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          ElevatedButton(
-            onPressed: (){
-              showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  // Use the BottomSheetContent widget as the content of the bottom sheet
-                  return BottomSheetContent();
-                },
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              elevation: 0,
-              fixedSize: Size(171, 32),
-              backgroundColor: Color(0xFF25C26E),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
-            child: Text(
-              'Buy',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontFamily: 'Satoshi',
-                fontWeight: FontWeight.w700,
-                height: 0.12,
-              ),
-            ),
+          _buildActionButton(
+            label: 'Buy',
+            onPressed: () => _showBuyBottomSheet(context),
+            backgroundColor: const Color(0xFF25C26E),
           ),
-
-
-          SizedBox(width: 16),
-
-          ElevatedButton(
-            onPressed: (){},
-            style: ElevatedButton.styleFrom(
-              elevation: 0,
-              fixedSize: Size(171, 32),
-              backgroundColor: Color(0xFFFF5449),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
-            child: Text(
-              'Sell',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontFamily: 'Satoshi',
-                fontWeight: FontWeight.w700,
-                height: 0.12,
-              ),
-            ),
+          const SizedBox(width: 16),
+          _buildActionButton(
+            label: 'Sell',
+            onPressed: () => _showSellBottomSheet(context),
+            backgroundColor: const Color(0xFFFF5449),
           ),
         ],
       ),
     );
+  }
+
+  ElevatedButton _buildActionButton({
+    required String label,
+    required VoidCallback onPressed,
+    required Color backgroundColor,
+  }) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        fixedSize: const Size(171, 32),
+        backgroundColor: backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontFamily: 'Satoshi',
+          fontWeight: FontWeight.w700,
+          height: 0.12,
+        ),
+      ),
+    );
+  }
+
+  void _showBuyBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return BottomSheetContent();
+      },
+    );
+  }
+
+  void _showSellBottomSheet(BuildContext context) {
+    // Handle the sell action
   }
 }
